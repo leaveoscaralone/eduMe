49a49756ac910263558b6c828b7118e4590251f4
leaveoscaralone <correawade@gmail.com>
2022-04-12 16:30:54 +0100

    algorithm working

--- server/controller/translator.js
+++ server/controller/translator.js
@@ -1,3 +1,36 @@
-exports.numToWords = async (req, res, next) => {
-    
+
+const numLetters = {
+    0: [''],
+    1: [''],
+    2: ['a', 'b', 'c'],
+    3: ['d', 'e', 'f'],
+    4: ['g', 'h', 'i'],
+    5: ['j', 'k', 'l'],
+    6: ['m', 'n', 'o'],
+    7: ['o', 'p', 'q', 's'],
+    8: ['t', 'u', 'v'],
+    9: ['w', 'x', 'y', 'z'],
 }
+
+function numToWord(digits) {
+    if (!digits.length) return [];
+    const digitsArr = digits.split('')
+    const lettersArr = digitsArr.map(digit => numLetters[digit])
+    let combinations = [''];
+    for (let i = 0; i < lettersArr.length; i++) {
+        let digitLetters = lettersArr[i];
+        let tempArr = []
+        for (let j = 0; j < digitLetters.length; j++) {
+            let currentLetter= digitLetters[j];
+            for (let k = 0; k < combinations.length; k++) {
+                let combination = combinations[k]
+                tempArr.push(combination + currentLetter)
+            }
+        }
+    combinations = tempArr
+    }
+    return combinations
+}
+
+
+module.exports = numToWord;
